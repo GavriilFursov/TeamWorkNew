af311515136c8938dd3d9393edfb357b0177f386
KosKazzz <xuzzzz@gmail.com>
2022-08-04 20:58:46 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] products = {"Хлеб", "Молоко", "Мясо", "Картофель", "Конфета"};
+        int[] prices = {56, 79, 550, 65, 200};
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + ". " + products[i] + " - " + prices[i] + " руб/шт.");
+        }
+        int[] countOfProducts = new int[products.length];
+        boolean[] fillOrNot = new boolean[products.length];
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберите номер товара и количество или для выхода наберите \"end\" .");
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            if (input.isEmpty()) {
+                System.out.println("Надо что-нибудь ввести =)");
+            }
+            if (input.length() < 3) {
+                System.out.println("Надо вводить правильно(номер продукта пробел количество!)");
+                continue;
+            }
+            int nomOfProd;
+            int countOfProd;
+            if (input.contains(" ")) {
+                if (input.startsWith(" ") || input.endsWith(" ")) {
+                    System.out.println("Надо вводить правильно(номер продукта пробел количество!)");
+                } else {
+                    int countOfRegex = 0;
+                    for (int i = 1; i < input.length() - 1; i++) {
+                        if (input.charAt(i) == ' ') {
+                            countOfRegex++;
+                        }
+                    }
+                    if (countOfRegex >= 2) {
+                        System.out.println("Ввели лишнее.Надо вводить правильно(номер продукта пробел количество!)");
+                        continue;
+                    }
+                    String[] parts = input.split(" ");
+                    try {
+                        nomOfProd = Integer.parseInt(parts[0]);
+                        countOfProd = Integer.parseInt(parts[1]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Надо вводить цифры...");
+                        continue;
+                    }
+                    if (nomOfProd > products.length || nomOfProd <= 0) {
+                        System.out.println("Не верный номер продукта!");
+                        continue;
+                    }
+                    if (countOfProd < 0 || countOfProd > 100) {
+                        System.out.println(" Введено не приемлемое количество продуктов!");
+                        continue;
+                    }
+
+                    countOfProducts[nomOfProd - 1] += countOfProd;
+                    fillOrNot[nomOfProd - 1] = true;
+                }
+
+            } else {
+                System.out.println("Необходимо номер продукта и количество разделить пробелом!");
+            }
+        }
+        System.out.println("Ваша корзина :");
+        int count = 1;
+        int sum = 0;
+        for (int i = 0; i < products.length; i++) {
+            if (fillOrNot[i]) {
+                System.out.println(count + ". " + products[i] + "-"
+                        + countOfProducts[i] + "шт. "
+                        + prices[i] + " руб/шт., "
+                        + (countOfProducts[i] * prices[i]) + " руб. в сумме;");
+                count++;
+                sum += countOfProducts[i] * prices[i];
+            }
+        }
+        System.out.println("  Итого: " + sum + " руб.");
+    }
+}
